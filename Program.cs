746399a2ed771ad40429efd2214f8b5fea7af5d4
semgl <semgl@GLEB280825PC>
2026-03-07 19:38:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "DNA";
+        Console.Write("Введите длину текста: ");
+        if (!int.TryParse(Console.ReadLine(), out int length) || length <= 0)
+        {
+            Console.WriteLine("Ошибка: нужно ввести положительное целое число.");
+            return;
+        }
+
+        char[] chars = { 'A', 'T', 'G', 'C' };
+        var rand = new Random();
+        var sb = new StringBuilder();
+        Console.WriteLine("\nРезультат:");
+
+        for (int i = 0; i < length; i++)
+        {
+            char symbol = chars[rand.Next(chars.Length)];
+            sb.Append(symbol);
+
+            switch (symbol)
+            {
+                case 'A': Console.ForegroundColor = ConsoleColor.Green; break;
+                case 'T': Console.ForegroundColor = ConsoleColor.Red; break;
+                case 'G': Console.ForegroundColor = ConsoleColor.Yellow; break;
+                case 'C': Console.ForegroundColor = ConsoleColor.Blue; break;
+            }
+
+            Console.Write(symbol);
+        }
+
+        Console.ResetColor();
+        Console.WriteLine("\n");
+
+        try
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, "dna_result.txt");
+
+            File.WriteAllText(filePath, sb.ToString());
+
+            Console.WriteLine($"Файл сохранен: {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при сохранении: {ex.Message}");
+        }
+
+        Console.WriteLine("Программа завершена. Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DNA 1.0.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DNA 1.0.0")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b2ff59a6-0194-4969-a096-091945396515")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
