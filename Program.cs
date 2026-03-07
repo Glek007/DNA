008eb7adb4003efc26deb963b3756d81b4a8b621
Glek007 <gleb00semer@gmail.com>
2026-03-07 19:42:13 +0300

    Code refactoring

--- Program.cs
+++ Program.cs
@@ -13,17 +13,14 @@ class Program
             Console.WriteLine("Ошибка: нужно ввести положительное целое число.");
             return;
         }
-
         char[] chars = { 'A', 'T', 'G', 'C' };
         var rand = new Random();
         var sb = new StringBuilder();
         Console.WriteLine("\nРезультат:");
-
         for (int i = 0; i < length; i++)
         {
             char symbol = chars[rand.Next(chars.Length)];
             sb.Append(symbol);
-
             switch (symbol)
             {
                 case 'A': Console.ForegroundColor = ConsoleColor.Green; break;
@@ -31,27 +28,18 @@ class Program
                 case 'G': Console.ForegroundColor = ConsoleColor.Yellow; break;
                 case 'C': Console.ForegroundColor = ConsoleColor.Blue; break;
             }
-
             Console.Write(symbol);
         }
-
         Console.ResetColor();
         Console.WriteLine("\n");
-
         try
         {
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             string filePath = Path.Combine(desktopPath, "dna_result.txt");
-
             File.WriteAllText(filePath, sb.ToString());
-
             Console.WriteLine($"Файл сохранен: {filePath}");
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Ошибка при сохранении: {ex.Message}");
-        }
-
+        catch (Exception ex) { Console.WriteLine($"Ошибка при сохранении: {ex.Message}"); }
         Console.WriteLine("Программа завершена. Нажмите любую клавишу для выхода...");
         Console.ReadKey();
     }
